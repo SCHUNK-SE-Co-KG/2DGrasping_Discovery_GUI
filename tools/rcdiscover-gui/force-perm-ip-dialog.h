7320f57f58f46e76f221b1fe641b1515668c7b6c
disa <divya.sasidharan@de.schunk.com>
2024-05-17 16:08:38 +0200

    Add permanent IP set functionality

---
+++ tools/rcdiscover-gui/force-perm-ip-dialog.h
@@ -0,0 +1,60 @@
+/*
+* Roboception GmbH
+* Munich, Germany
+* www.roboception.com
+*
+* Copyright (c) 2017 Roboception GmbH
+* All rights reserved
+*
+* Author: Raphael Schaller
+*/
+#ifndef FORCE_PERM_IP_DIALOG_H
+#define FORCE_PERM_IP_DIALOG_H
+
+#include "sensor-command-dialog.h"
+
+#include <map>
+
+/**
+ * @brief Dialog for sending FORCEPERMIP_CMD to camera.
+ */
+class ForcePermIpDialog : public SensorCommandDialog
+{
+  public:
+    ForcePermIpDialog() = default;
+
+    ForcePermIpDialog(wxHtmlHelpController *help_ctrl,
+                  wxWindow *parent, wxWindowID id,
+                  const wxPoint &pos = wxDefaultPosition,
+                  long style = wxDEFAULT_DIALOG_STYLE,
+                  const wxString &name = wxDialogNameStr);
+
+    virtual ~ForcePermIpDialog() = default;
+
+  private:
+    void addIpToBoxSizer(wxBoxSizer *sizer,
+                         std::array<wxTextCtrl *, 4> &ip, int id);
+    static std::uint32_t parseIp(const std::array<wxTextCtrl *, 4> &ip);
+
+    void changeTextCtrlIfNotChangedByUser(wxTextCtrl *ctrl,
+                                          const std::string &v);
+
+  private:
+    void onClearButton(wxCommandEvent &event);
+
+    void onForcePermIpButton(wxCommandEvent &event);
+    void onHelpButton(wxCommandEvent &event);
+
+    void onPermIpChanged(wxCommandEvent &event);
+
+  private:
+    std::array<wxTextCtrl *, 4> ip_;
+    std::array<wxTextCtrl *, 4> subnet_;
+    std::array<wxTextCtrl *, 4> gateway_;
+
+    std::map<const wxTextCtrl *, bool> changed_by_user_;
+
+    wxDECLARE_EVENT_TABLE();
+};
+
+#endif // FORCE_PERM_IP_DIALOG_H
